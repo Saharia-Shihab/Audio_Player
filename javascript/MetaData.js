15317a6a5a60e2b47f19c347992334cb2710b1a6
Arif Saharia Shihab <GitHubshihab@gmail.com>
2022-02-16 20:18:03 +0600

    reWrite the whole Player v0.11

--- javascript/MetaData.js
+++ javascript/MetaData.js
@@ -196,6 +196,13 @@ export default [
         Released: 2011,
         Image: img('fPO76Jlnz6c'),
         _src: 'https://cdn.discordapp.com/attachments/941340630792171540/943399266364297216/y2mate.com_-_Coolio_Gangstas_Paradise_feat_LV_Official_Music_Video.mp3'
+    }, {
+        Title: "Young Shahrukh",
+        Artist: "Tesher",
+        Album: "Young Shahrukh",
+        Released: 2020,
+        Image: img('6nEc9455oJk'),
+        _src: 'https://cdn.discordapp.com/attachments/941340630792171540/943447003281625168/y2mate.com_-_Tesher_YOUNG_SHAHRUKH.mp3',
     }
 ].sort(function (a, b) {
     if (a.Title < b.Title) { return -1; }
